d3f9c579f3796fddbfa745f4c7fc7e86033c2fd5
Michele Moretti <cicobalico88@gmail.com>
2017-07-05 19:04:05 +0200

    Fix TimeDistributed layer wrapper
    The TimeDistributed wrapper needs to have a layer as an argument, not a tensor

--- keras_rcnn/models.py
+++ keras_rcnn/models.py
@@ -8,16 +8,13 @@ class RCNN(keras.models.Model):
     def __init__(self, inputs, classes, regions_of_interest):
         y = keras_rcnn.layers.ROI(14, regions_of_interest)(inputs)
 
-        y = keras.layers.Dense(4096)(y)
-        y = keras.layers.TimeDistributed(y)
+        y = keras.layers.TimeDistributed(keras.layers.Dense(4096))(y)
 
         score = keras.layers.Dense(classes)(y)
-        score = keras.layers.Activation("softmax")(score)
-        score = keras.layers.TimeDistributed(score)
+        score = keras.layers.TimeDistributed(keras.layers.Activation("softmax"))(score)
 
         boxes = keras.layers.Dense(4 * (classes - 1))(y)
-        boxes = keras.layers.Activation("linear")(boxes)
-        boxes = keras.layers.TimeDistributed(boxes)
+        boxes = keras.layers.TimeDistributed(keras.layers.Activation("linear"))(boxes)
 
         super(RCNN, self).__init__(inputs, [score, boxes])
 
